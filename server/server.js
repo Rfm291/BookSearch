c5a842841616433c18b88b6505e64cce6ad5bd96
Richard Mercado <RichardFM291@gmail.com>
2021-08-26 21:37:37 -0400

    modified mutations and pkg json

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-//const routes = require('./routes');
+const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
